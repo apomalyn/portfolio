62776d0c6584c51e7f99e4a569f4394f432f646c
Xavier Chrétien <chretienxavier42@gmail.com>
2020-12-06 23:52:39 -0500

    Add 'scroll' on l10n files

--- lib/widgets/scroll_down_animated.dart
+++ lib/widgets/scroll_down_animated.dart
@@ -1,5 +1,6 @@
 import 'dart:math' as math;
 import 'package:flutter/material.dart';
+import 'package:portfolio/generated/l10n.dart';
 import 'package:portfolio/utils/app_theme.dart';
 
 class ScrollDownAnimated extends StatefulWidget {
